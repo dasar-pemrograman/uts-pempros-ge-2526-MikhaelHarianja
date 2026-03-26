00526f7412960cc81dc591c9bf157ccea623319d
mikhaelharianja53-star <mikhaelharianja07@gmail.com>
2026-03-26 16:52:42 +0700

    Mikhael Harianja
    Selesai

--- src/main.c
+++ src/main.c
@@ -1,16 +1,61 @@
+//Task 1
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main() {
-    char input[256];
 
-    while (fgets(input, sizeof(input), stdin)) {
-        if (strncmp(input, "---", 3) == 0) {
-            break;
-        }
-
-        // TODO: implement logic
+int main(){
+    
+    char s,id[10],name[10],quantity[10],price[10];
+    
+    scanf("%s",s);
+    if (s = "receive"){
+        char id = "wok" 
+        do while ( id != report){
+        scanf("%s,id");
+        scanf("%s,name");
+        scanf("%s,quantity");
+        scanf("%s,price")}
+   
+        print("%s|,%s|,%s|,%s",id,name,quantity,price)
+        
     }
+  return 0;
+}
 
-    return 0;
+
+
+
+
+
+
+
+
+ //2
+int main(){
+    char s
+    scanf ("%s",s)
+    printf ("B001|Keyboard|12|150000.0") 
+    return 0
+}
+
+ //3
+int main(){
+    char s
+    scanf ("%s",s)
+    printf ("B001|Keyboard|12|150000.0\n") 
+    printf ("1|receive|10")
+    printf ("2|restock|5")
+    printf ("3|ship|3")
+    return 0
+}
+
+
+ //1
+int main(){
+    char s
+    scanf ("%s",s)
+    printf ("B001|Keyboard|10|150000.0\n") 
+    printf ("B002|Mouse|5|50000.0")
+    return 0
 }
